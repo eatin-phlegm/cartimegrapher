1e356bc45ea37eb059d6e3f9afbb902215a7134d
Ethan Fleming <39923214+eatin-phlegm@users.noreply.github.com>
2019-09-11 12:47:10 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 setup(
     name='cartimegrapher',
     version='0.0.1',
-    packages=find_packages(),
+    packages=['cartimegrapher'],
     url='https://github.com/eatin-phlegm/cartimegrapher',
     license='GNUv3.0',
     author='eatin-phlegm',
